6416dec48d175ae25371bed4807edb21fd101222
pankaj <pandeypankaj1986@gmail.com>
2024-07-27 21:44:51 +0530

    First commit

---
+++ src/test/java/com/example/demo/FirstProject.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirstProject {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello! My Spring Boot First Program");
+
+	}
+
+}
